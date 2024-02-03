56ee30e33280e9ebb178d8cbfdc4e4abd0d9876b
Andy Dam <dam.andy.2411@gmail.com>
2024-02-02 23:31:04 -0600

    initiallized class to sort files

--- download_sort.py
+++ download_sort.py
@@ -1,9 +1,21 @@
 import os
 
+class FileSorter:
+    def __init__(self, path):
+        self.path = path
+        self.files = [f for f in os.listdir(path) if os.path.isfile(path + f)]
+        self.folders = [f for f in os.listdir(path) if os.path.isdir(path + f)]
+
+    def sort_file(self, file):
+        pass
+
 def main():
     path = "C:\\Users\\" + os.getlogin() + "\\Downloads\\"
-    files = [f for f in os.listdir(path) if os.path.isfile(path + f)]
-    print(files)
+    unsorted_files = FileSorter(path)
+    for file in unsorted_files.files:
+        print(file)
+    print(unsorted_files.files)
+    print(unsorted_files.folders)
     
 if __name__ == "__main__":
     main()
