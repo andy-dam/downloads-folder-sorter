bdab722792484c290e87df5b89d16c57f20ad7a4
Andy Dam <dam.andy.2411@gmail.com>
2024-02-11 14:29:12 -0600

    completed sorting function and exported to .exe

--- download_sort.py
+++ download_sort.py
@@ -1,4 +1,5 @@
 import os
+import filetype
 
 class FileSorter:
     def __init__(self, path):
@@ -6,16 +7,29 @@ class FileSorter:
         self.files = [f for f in os.listdir(path) if os.path.isfile(path + f)]
         self.folders = [f for f in os.listdir(path) if os.path.isdir(path + f)]
 
-    def sort_file(self, file):
-        pass
-
+    def sort_file(self, file) -> str:
+        extension = filetype.guess(self.path + file)
+        current_path = self.path + file
+        if extension is None:
+            if not os.path.exists(self.path + "Miscellaneous"):
+                os.makedirs(self.path + "Miscellaneous")
+            new_path = self.path + "Miscellaneous\\" + file
+        else:
+            extension = extension.extension
+            if not os.path.exists(self.path + extension):
+                os.makedirs(self.path + extension)
+            new_path = self.path + extension + "\\" + file
+        os.rename(current_path, new_path)
+        return new_path
+    
+    def sort_files(self):
+        for file in self.files:
+            self.sort_file(file)
+    
 def main():
     path = "C:\\Users\\" + os.getlogin() + "\\Downloads\\"
     unsorted_files = FileSorter(path)
-    for file in unsorted_files.files:
-        print(file)
-    print(unsorted_files.files)
-    print(unsorted_files.folders)
+    print(path)
     
 if __name__ == "__main__":
     main()
